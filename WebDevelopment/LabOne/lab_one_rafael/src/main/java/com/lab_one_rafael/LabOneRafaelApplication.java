279bfc359ebda8829fd93af930af888bd3943198
Rafael Perez <rafaelandresph@gmail.com>
2025-01-16 13:36:55 -0500

    Add all folders for labs and homework, only the intro, homework one (in progress), and lab one (finished) is there

---
+++ WebDevelopment/LabOne/lab_one_rafael/src/main/java/com/lab_one_rafael/LabOneRafaelApplication.java
@@ -0,0 +1,13 @@
+package com.lab_one_rafael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabOneRafaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabOneRafaelApplication.class, args);
+	}
+
+}
---
+++ WebDevelopment/LabOne/lab_one_rafael/src/test/java/com/lab_one_rafael/LabOneRafaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab_one_rafael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabOneRafaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
