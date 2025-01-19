6bed7ac483a91d8a2c3848fab35a38062af89f69
Rafael Perez <rafaelandresph@gmail.com>
2025-01-19 17:37:08 -0500

    add css external style, edit html page with style, remove space in controller

--- WebDevelopment/HW01/rodriguez_web/src/main/java/com/rodriguez_web/Controller.java
+++ WebDevelopment/HW01/rodriguez_web/src/main/java/com/rodriguez_web/Controller.java
@@ -7,10 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class Controller {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/list")
     public String helloController() {
-        return "<h1>Hello World</h1>";
+        return "<h1>Hello Readers, here are the list of books I'm reading :)</h1>";
     }
-    
-    
 }
